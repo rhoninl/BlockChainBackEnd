26f50cf572ecfe0c1006d7881645b623037ef183
mrleea <a964581583@hotmail.com>
2022-04-20 20:10:30 +0800

    fix cors

--- MiddleWare/Auth.go
+++ MiddleWare/Auth.go
@@ -2,7 +2,6 @@ package MiddleWare
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 	"main/Utils"
 	"net/http"
 	"sync"
@@ -41,27 +40,16 @@ func Auth() gin.HandlerFunc {
 }
 
 func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin")
-		if origin != "" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-			c.Header("Access-Control-Allow-Method", "POST,GET,OPTIONS,PUT,DELETE,UPDATE")
-			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token, session")
-			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
-			c.Header("Access-Control-Max-Age", "172800")
-			c.Header("Access-Control-Allow-Credentials", "true")
-			c.Header("Access-Control-Allow-Origin", "*")
-		}
+	return func(context *gin.Context) {
+		method := context.Request.Method
+		context.Header("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		context.Header("Access-Control-Allow-Credentials", "true")
 		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
+			context.AbortWithStatus(http.StatusNoContent)
 		}
-		defer func() {
-			if err := recover(); err != nil {
-				log.Printf("Panic info is: %v", err)
-			}
-		}()
-		c.Next()
+		context.Next()
 	}
-
 }
