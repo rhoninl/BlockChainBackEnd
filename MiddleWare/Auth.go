b0894b461cde928161e679df6b879e798f2cb49a
mrleea <a964581583@hotmail.com>
2022-04-25 19:42:32 +0800

    update

--- MiddleWare/Auth.go
+++ MiddleWare/Auth.go
@@ -10,33 +10,33 @@ import (
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token, err := c.Cookie("token")
+		//if err == nil {
+		//获取CompanyId
+		info, err := Utils.ParseToken(token)
 		if err == nil {
-			//获取CompanyId
-			info, err := Utils.ParseToken(token)
-			if err == nil {
-				info.CompanyId = 1
-			}
-			//获取成功则向后传输CompanyId
-			c.Set("companyId", info.CompanyId)
-			wg := sync.WaitGroup{} //同步
-			//协程执行闭包中的 续杯程序
-			go func(info *Utils.CustomClaims) {
-				wg.Add(1)
-				defer wg.Done()
-				t := time.Unix(info.ExpiresAt, 0)
-				timeExceed := int(t.Sub(time.Now()).Seconds())
-				// 当token有效期小于一半时，生成新的token
-				if timeExceed < Utils.MAXAGE/2 {
-					c.SetCookie("token", Utils.CreateToken(info.CompanyId), Utils.MAXAGE, "/", "", false, false)
-				}
-			}(info)
-
-			c.Next() //继续执行后续程序
-			wg.Wait()
-			return
-			//}
-			//c.Set("companyId", )
+			info.CompanyId = 1
 		}
+		//获取成功则向后传输CompanyId
+		c.Set("companyId", info.CompanyId)
+		wg := sync.WaitGroup{} //同步
+		//协程执行闭包中的 续杯程序
+		go func(info *Utils.CustomClaims) {
+			wg.Add(1)
+			defer wg.Done()
+			t := time.Unix(info.ExpiresAt, 0)
+			timeExceed := int(t.Sub(time.Now()).Seconds())
+			// 当token有效期小于一半时，生成新的token
+			if timeExceed < Utils.MAXAGE/2 {
+				c.SetCookie("token", Utils.CreateToken(info.CompanyId), Utils.MAXAGE, "/", "", false, false)
+			}
+		}(info)
+
+		c.Next() //继续执行后续程序
+		wg.Wait()
+		return
+		//}
+		//c.Set("companyId", )
+		//}
 		//c.JSON(http.StatusBadRequest, gin.H{"message": "请先登陆"})
 		//c.Abort()
 		//}
