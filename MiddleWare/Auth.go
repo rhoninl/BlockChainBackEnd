a70de224486694d8d7a42e072864642a7ba3973f
mrleea <a964581583@hotmail.com>
2022-04-26 12:05:51 +0800

    update

--- MiddleWare/Auth.go
+++ MiddleWare/Auth.go
@@ -1,10 +1,9 @@
 package MiddleWare
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"main/Utils"
-	"sync"
-	"time"
 )
 
 func Auth() gin.HandlerFunc {
@@ -16,24 +15,26 @@ func Auth() gin.HandlerFunc {
 		var companyId = int64(1)
 		if token != "" && err == nil {
 			companyId = info.CompanyId
+		} else {
+			fmt.Println("有个小比崽子没登陆")
 		}
 		//获取成功则向后传输CompanyId
 		c.Set("companyId", companyId)
-		wg := sync.WaitGroup{} //同步
+		//wg := sync.WaitGroup{} //同步
 		//协程执行闭包中的 续杯程序
-		go func(info *Utils.CustomClaims) {
-			wg.Add(1)
-			defer wg.Done()
-			t := time.Unix(info.ExpiresAt, 0)
-			timeExceed := int(t.Sub(time.Now()).Seconds())
-			// 当token有效期小于一半时，生成新的token
-			if timeExceed < Utils.MAXAGE/2 {
-				//c.SetCookie("token", Utils.CreateToken(info.CompanyId), Utils.MAXAGE, "/", "", false, false)
-				//http.SetCookie(c.Writer, &http.Cookie{})
-			}
-		}(info)
+		//go func(info *Utils.CustomClaims) {
+		//	wg.Add(1)
+		//	defer wg.Done()
+		//	t := time.Unix(info.ExpiresAt, 0)
+		//	timeExceed := int(t.Sub(time.Now()).Seconds())
+		//	// 当token有效期小于一半时，生成新的token
+		//	if timeExceed < Utils.MAXAGE/2 {
+		//		//c.SetCookie("token", Utils.CreateToken(info.CompanyId), Utils.MAXAGE, "/", "", false, false)
+		//		//http.SetCookie(c.Writer, &http.Cookie{})
+		//	}
+		//}(info)
 		c.Next() //继续执行后续程序
-		wg.Wait()
+		//wg.Wait()
 		return
 		//}
 	}
